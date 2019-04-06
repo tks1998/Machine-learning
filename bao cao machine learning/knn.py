859afc9f6f56778345c38d1ec88982ab72e273c7
tks1998 <trankimsen1681998@gmail.com>
2019-04-06 13:48:48 +0700

    knn.py

--- bao cao machine learning/knn.py
+++ bao cao machine learning/knn.py
@@ -10,23 +10,33 @@ A = np.genfromtxt('data.txt', delimiter=',')
  
 
 trainX = A[:60,0:5]
-trainY = A[:60,5]
-trainX = scaling2(trainX)
+p = np.array(np.zeros(60)) 
+for i in range (0,60):
+    p[i] = np.sqrt((np.sum(A[i]*A[i])))
 
+trainY = A[:60,5]
+t = np.mean(trainX,axis = 0 ) 
+st = np.std(trainX,axis = 0)
+   
+#trainX = scaling2(trainX,t,st)
+trainX=scaling(trainX,p)
 #print(trainX)
 #print(trainY)
 testX = A[60:80,0:5]
-testX = scaling2(testX)
+#testX = scaling2(testX,t,st)
+testX = scaling(testX,p)
+print(testX)
 testY = A[60:80,5]
 k = 5
 p = 2 
 build = computing_knn(trainX,trainY,testX,k,p)
 
-# output f1_score ,  accuracy ,presicion 
+# output f1_score ,  accuracy ,presicion i
 
 ouput(build,testY,k)
 
 #print ( build )
+
 
 
 
